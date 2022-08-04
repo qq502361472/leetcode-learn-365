b62e22f235e443582c428e5d5266a7fceb54519a
胡健 <502361472@qq.com>
2022-08-04 14:16:24 +0800

    Initial commit

---
+++ src/main/java/com/hjrpc/array/Question1.java
@@ -0,0 +1,36 @@
+package com.hjrpc.array;
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ * 示例 1：
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 示例 2：
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 示例 3：
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * nums 是一个非递减数组
+ * -109 <= target <= 109
+ */
+public class Question1 {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] result = searchRange(nums, target);
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+
+        return null;
+    }
+}
